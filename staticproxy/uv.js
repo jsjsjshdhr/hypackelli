94a0f5d97dbbd2c17fcaa07e18ba34975ecf8458
GoogleCreator <161095604+GoogleCreator@users.noreply.github.com>
2025-04-25 20:03:12 -0700

    Delete staticproxy directory

--- staticproxy/uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('/staticproxy/uv/uv.bundle.js');
-importScripts('/staticproxy/uv/uv.config.js');
-importScripts('/staticproxy/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
