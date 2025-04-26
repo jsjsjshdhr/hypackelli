1e540ed6bfa1602fb4e13d37f2c564aec8dd1e30
GoogleCreator <161095604+GoogleCreator@users.noreply.github.com>
2025-04-25 20:02:44 -0700

    Delete oldassets directory

--- oldassets/js/nodrag.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const images = document.querySelectorAll('img');
-
-  images.forEach((image) => {
-    image.setAttribute('draggable', 'false');
-  });
-});
